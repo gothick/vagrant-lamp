2cd6e567cf1a58d5be420a76061738a26791cda4
Matt Gibson <gothick@gothick.org.uk>
2017-08-09 22:12:10 +0100

    Fix up MySQL install so we use our now-standard no-password-but-root- can-get-in-anyway setup. Add php-xml install so morning-crossword works, though I can't even remember which bit was moaning about it. Probably the querypath library.

--- provision.sh
+++ provision.sh
@@ -35,7 +35,7 @@ apt-get -y install build-essential binutils-doc git emacs24-nox
 # Install Apache
 apt-get -y install apache2
 # And all the php things.
-apt-get -y install php7.0 php7.0-curl php7.0-mysql php7.0-sqlite php-xdebug php7.0-gd libapache2-mod-php7.0
+apt-get -y install php7.0 php7.0-curl php7.0-mysql php7.0-sqlite php-xdebug php7.0-gd libapache2-mod-php7.0 php-xml
 # Also want Imagemagick for various helpful manipulations
 apt-get -y install imagemagick php-imagick
 # And our crossword site uses QPDF and I can't be bothered
@@ -55,15 +55,18 @@ xdebug.remote_host=10.0.2.2
 EOF
 
 # Install MySQL
-echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+# MG The DEBIAN_FRONTENT=noninteractive flag above solved this problem for us, combined
+# with the upgrade to Ubuntu 16.04 we now have a version of MySQL where if you're root,
+# it won't bother asking you for a password, which is fine for my purposes.
+#echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+#echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
 apt-get -y install mysql-client mysql-server
 
 sed -i "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" ${mysql_config_file}
 
-# Allow root access from any host
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION" | mysql -u root --password=root
-echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql -u root --password=root
+# Allow root access from any host. We shouldn't need a user/password for MySQL if we're running as root.
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION" | mysql
+echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql
 
 # Overwrite default site config with one that allows htaccess override
 cp /vagrant/vm_files/000-default.conf /etc/apache2/sites-available/000-default.conf
